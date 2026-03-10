ff7816e2e7481ab7a2aa1e16e7f6bbee741b65d1
abdelra7man <abdelra7manabdullah@gmail.com>
2026-03-10 23:43:38 +0200

    FEAT(servoce): Adding File Parser Service up read file content according to file type

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,11 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UploadFileRequest;
+use App\Services\FileParserService;
 
 class ArticleController extends Controller
 {
+    public function __construct(
+        public FileParserService $fileParserService
+    ){}
+
    public function upload(UploadFileRequest $request)
    {
+        $file = $request->file('file');
 
+        try {
+            $content = $this->fileParserService->extract($file);
+
+            return response()->json(['content' => $content], 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Failed to parse file: ' . $e->getMessage()], 500);
+        }
+     
    }
 }
---
+++ app/Services/FileParserService.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Http\UploadedFile;
+use League\Csv\Reader;
+use Smalot\PdfParser\Parser;
+
+class FileParserService
+{
+    public function extract(UploadedFile $file)
+    {
+        $fileExtension = $this->getFileType($file);
+        $filePath = $file->getPathname();
+        match ($fileExtension) {
+            'txt','md' => $this->extractText($file),
+            'csv' => $this->extractCsv($file),
+            'pdf' => $this->extractPdf($file),
+            default => throw new \RuntimeException("Unsupported file type: $fileExtension"),
+        };
+       
+    }
+
+    private function getFileType(UploadedFile $file): string
+    {
+        return  strtolower( $file->getClientOriginalExtension() );
+    }
+
+    private function extractText(string $file): string
+    {
+        return file_get_contents($file);
+    }
+
+    private function extractCsv(string $file): string
+    {
+        $csv = Reader::from($file, 'r');
+        $csv->setHeaderOffset(0); // Assuming the first row contains headers
+
+        $lines = [];
+        $lines[] = implode(' | ', $csv->getHeader()); // Add headers as the first line
+
+        foreach ($csv->getRecords() as $row) {
+            $lines[] = implode(' | ', $row);
+        }
+        return implode("\n", $lines);
+
+    }
+
+    private function extractPdf(string $file)
+    {
+        $parser = new Parser(); 
+        $pdf = $parser->parseFile($file);
+        if (!$pdf) {
+            throw new \RuntimeException("Failed to parse PDF file.");
+        }
+
+        $pdfText =  $pdf->getText();
+        if (trim($pdfText) === '') {
+                throw new \RuntimeException(
+                'Could not extract text from PDF. ' .
+                'The file may be scanned/image-based.'
+            );
+        }
+        return $pdfText;
+
+    }
+}
