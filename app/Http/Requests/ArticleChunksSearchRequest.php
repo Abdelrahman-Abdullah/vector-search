9b02a494495d860429470398d2d547bf43161d4a
abdelra7man <abdelra7manabdullah@gmail.com>
2026-03-11 07:58:32 +0200

    FEAT(form-request): Adding new form requests for vector searchs

---
+++ app/Http/Requests/ArticleChunksSearchRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ArticleChunksSearchRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'q' => ['required', 'string', 'min:2', 'max:2000'],
+            'top' => ['nullable', 'integer', 'min:1', 'max:50'],
+        ];
+    }
+}
