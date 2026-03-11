db51e896167a005936693e2da1114ac6837e183e
abdelra7man <abdelra7manabdullah@gmail.com>
2026-03-11 08:14:58 +0200

    UPDATE(api): Adding Rate Limiter for endpoints

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
-// Public API endpoints. Add auth middleware here when the client auth flow is ready.
 Route::middleware('throttle:30,1')->group(function () {
 	Route::post('/articles/upload', [ArticleController::class, 'upload']);
 	Route::get('/articles/search/vector', [ArticleController::class, 'vectorSearch']);
